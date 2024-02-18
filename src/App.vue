eb89d8d308ace19f7f6e603bd8a26764e8ddcdda
wgithub24 <wgithub24@gmail.com>
2024-02-18 23:22:34 +0630

    chapter4 vue component&slot directive

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1 ref="greeting"> Hello{{name}}</h1>
+  <div v-if="showModal">
+    <modal @close="showModal=false" :theme="theme">
+      <h1>Login</h1>
+      <p>Hi There</p>
+      <template v-slot:links>
+      <a href="">Google</a>
+      <a href="">Google</a>
+      </template>
+
+    </modal>
+  </div>
+   <div v-if="showModal1">
+    <modal @close="showModal1=false" :theme="theme2" >
+      <h1>Delete</h1>
+      <p>Hi There</p>
+      <template v-slot:links>
+      <a href="">Google</a>
+      <a href="">Google</a>
+      </template>
+
+    </modal>
+  </div>
+  <button @click="showModal='true'"> show </button>
+  <button @click="showModal1=true"> delete </button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+ data(){
+  return{
+   
+    showModal:false,
+    showModal1:false,
+    theme:"success",
+    theme2:"delete"
+    
+  }
+ },
+ components:{Modal}
+ 
+ 
+}
+</script>
+
+<style scoped>
+h1{
+  color:white;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="backdrop" @click.self="closeModel"  >
+    <div class="modal" :class="{success:theme==='success',danger:theme==='delete'}">
+       
+       <slot/>
+         <slot name="links"/>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['theme'],
+  methods:{
+    closeModel(){
+        this.$emit("close")
+    }
+  }
+}
+</script>
+
+<style>
+ .backdrop{
+    position:fixed;
+    top: 0;
+    background:rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+
+ }
+ .modal{
+ width: 400px;
+ padding: 20px;
+ margin: 100px auto;
+ background-color: aliceblue;
+ border-radius: 10px;
+
+ }
+ .success{
+    background-color: green;
+    color: white;
+ }
+ .danger{
+    background-color: red;
+    color: white;
+ }
+
+</style>
